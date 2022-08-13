243046ffd17a2a80ce4b11212fb0ea61ff9438b2
John Paxton <johnpaxton@gmail.com>
2022-08-13 10:23:22 -0400

    Modernize processing

--- scripts/processCanada.js
+++ scripts/processCanada.js
@@ -1,52 +1,52 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-    path = require('path'),
-    zips = {}, str,
-    data = fs.readFileSync('./ca_postal_codes.csv', 'utf8').replace(/\r/g, '').split('\n');
+let fs = require('fs');
+let path = require('path');
+let zips = {};
+let str;
+let data = fs.readFileSync('./ca_postal_codes.csv', 'utf8').replace(/\r/g, '').split('\n');
 
 data.shift();
 
-var clean = function(str) {
-    return str.replace(/"/g, '').trimLeft();
-}
-
-var ucfirst = function(str) {
-    str = str.toLowerCase();
-    var lines = str.split(' ');
-    lines.forEach(function(s, i) {
-        var firstChar      = s.charAt(0),
-            upperFirstChar = firstChar.toUpperCase();
-
-        lines[i] = upperFirstChar + s.substring(1);
-        
-    });
-    return lines.join(' ');
+let clean = function (str) {
+  return str.replace(/"/g, '').trimLeft();
 };
 
-data.forEach(function(line, num) {
-    line = line.split(',');
-    if (line.length > 1) {
-        var o = {};
+let ucfirst = function (str) {
+  str = str.toLowerCase();
+  let lines = str.split(' ');
+  lines.forEach(function (s, i) {
+    let firstChar = s.charAt(0);
+    let upperFirstChar = firstChar.toUpperCase();
 
-        o.zip = clean(line[0]);
-        o.latitude = Number(clean(line[3]));
-        o.longitude = Number(clean(line[4]));
-        o.city = ucfirst(clean(line[1]));
-        o.state = clean(line[2]);
-        o.country = 'Canada';
-        if (!zips[o.zip]) {
-            zips[o.zip] = o;
-        }
+    lines[i] = upperFirstChar + s.substring(1);
+  });
+  return lines.join(' ');
+};
+
+data.forEach(function (line, num) {
+  line = line.split(',');
+  if (line.length > 1) {
+    let o = {};
+
+    o.zip = clean(line[0]);
+    o.latitude = Number(clean(line[3]));
+    o.longitude = Number(clean(line[4]));
+    o.city = ucfirst(clean(line[1]));
+    o.state = clean(line[2]);
+    o.country = 'Canada';
+    if (!zips[o.zip]) {
+      zips[o.zip] = o;
     }
+  }
 });
 
-var stateMap = {};
+let stateMap = {};
 
-for (var i in zips) {
-    var item = zips[i];
-    stateMap[item.state] = stateMap[item.state] || [];
-    stateMap[item.state].push(item.zip);
+for (let i in zips) {
+  let item = zips[i];
+  stateMap[item.state] = stateMap[item.state] || [];
+  stateMap[item.state].push(item.zip);
 }
 
 str = 'exports.codes = ' + JSON.stringify(zips) + ';\n';
